c4fe9a7880be8e1327dcb5c0790539ed3ed4cccd
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-05-29 04:31:38 +0800

    :bento: Update favicon
    and update title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'Malaysia Prayer Time',
+    title: 'Malaysia Prayer Time App',
     tagline: 'Modern and easy to use',
     url: 'https://mywaktusolat.vercel.app',
     baseUrl: '/',
